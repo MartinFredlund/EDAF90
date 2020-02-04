cfd912124e64a8425b47f32ba6be41c72d0473bf
Martin Fredlund <ma1233fr-s@student.lu.se>
2020-02-04 15:56:09 +0100

    abit

--- lab2/src/ComposeSalad.js
+++ lab2/src/ComposeSalad.js
@@ -1,6 +1,25 @@
 import React from "react";
 
 class ComposeSalad extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+          foundation: 'Sallad',
+          protien: 'Kyckling',
+          extra: 'Avokado',
+          dressing: 'Ceasardressing'
+        };
+        this.stateProtien()
+        this.handleChange = this.handleChange.bind(this);
+    }
+    handleInputChange(event){
+        const target = target.event;
+    }
+    handleSubmit(event) {
+    alert('value: ' + this.stateFoundation.value);
+    event.preventDefault();
+    }
+
   render() {
     const inventory = this.props.inventory;
     // test for correct ussage, the parent must send this datastructure
@@ -10,17 +29,38 @@ class ComposeSalad extends React.Component {
     let foundations = Object.keys(inventory).filter(
       name => inventory[name].foundation
     );
+    let protiens = Object.keys(inventory).filter(
+        name => inventory[name].protien
+    );
     return (
-      <div className="container">
+     <form onSubmit={this.handleSubmit}>
+         <label>
+             Foundation:
+             <select value= {this.state.value} onChange={this.handleChange}>
+                <option value="Sallad">Sallad</option>
+                <option value="Pasta">Pasta</option>
+                <option value="Sallad + Pasta">Sallad och Pasta</option>
+                <option value="Sallad + Matvete">Sallad och Matvete</option>
+                </select>
+         </label>
+         <label>
+             Protien: 
+
+         </label>
+         <input type="submit" value="Submit"/>
+     </form>
+
+    );
+  }
+}
+
+export default ComposeSalad;
+
+{/* <div className="container">
         <h4>Välj bas</h4>
         <ul>
           {foundations.map(name => (
             <li key={name}>{name}</li>
           ))}
         </ul>
-      </div>
-    );
-  }
-}
-
-export default ComposeSalad;
+      </div> */}
