2d05b71cc5dc21eadfff9080dc8929b9fb44ac12
Martin Fredlund <ma1233fr-s@student.lu.se>
2020-02-04 12:43:34 +0100

    more setup

--- lab2/src/App.js
+++ lab2/src/App.js
@@ -1,21 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import inventory from ’./inventory.ES6’;
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+function App() {
+   return (
+     <div>
+       <div className="jumbotron text-center">
+         <h1>My Own Salad Bar</h1>
+         <p>Here you can order custom made salads!</p> 
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+       <div>
+         Add your own react component here!
+     </div>
+   </div>
+   );
 }
 
 export default App;
--- lab2/src/index.js
+++ lab2/src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import "bootstrap/dist/js/bootstrap.js"; // skip this if you do not use bootstrap modals
+import $ from 'jquery';                  // skip this if you do not use bootstrap modals
+import Popper from 'popper.js';          // skip this if you do not use bootstrap modals
 
 ReactDOM.render(
   <App />,
