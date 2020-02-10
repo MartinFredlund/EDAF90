f497cd6978348a30a5426ed1f5af42985357a918
filipbergman <fi5731be-s@student.lu.se>
2020-02-10 09:39:24 +0100

    små ändringar

--- lab2/src/Orders.js
+++ lab2/src/Orders.js
@@ -3,11 +3,9 @@ import React from 'react';
 class Orders extends React.Component {
     constructor(props) {
         super(props);
-        //console.log("Hej:" + this.props.order);
     }
 
     render() {
-        //console.log("Hej:" + this.props.order);
         return (
         <div className="container">
           <h4>Order</h4>
